effe48e4ae58fa840e4420c43a9857eaeb8d3eef
limy <mingyuli1997@gmail.com>
2023-09-17 23:20:07 +0800

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Practical
+
+整理 React 的一些最佳实践
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="page_main">
+      <h1>React Practical</h1>
+    </div>
+  );
+}
+
+export default App;
