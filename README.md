5dd8f471780733370e89a72f71acbea09b0a40cc
limy <mingyuli1997@gmail.com>
2023-09-19 22:28:48 +0800

    Revert "ref"
    This reverts commit f40f1ed1db647e8a085fcfb74762a64fe1db2770.

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # React Practical
 
-## React.createRef
+整理 React 的一些最佳实践
 
-常用于类组件 createRef 并没有 Hooks 的效果，其值会随着 FunctionComponent 重复执行而不断被初始化，每次拿到的引用都是新的，所以不建议放在函数式组件
-
-## React.useRef
-
-只能用于函数式组件
-
-## forwardRef & useImperativeHandle
-
-用于转发 Ref，一般用于父组件访问子组件的方法、元素。
+- [组件开发时，透传 style、className、HTMLAttributes](https://github.com/mingyuLi97/react-practical/blob/PropForwarding/src/Button.tsx)
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import UseRefDemo from "./useRefDemo";
-import CreateRef from "./CreateRef";
-import CreateAndUse from "./CreateAndUse";
-import Forward from "./Forward";
-
 function App() {
   return (
     <div className="page_main">
-      {/* <UseRefDemo></UseRefDemo>
-      <CreateRef></CreateRef> */}
-      {/* <CreateAndUse></CreateAndUse> */}
-      <Forward></Forward>
+      <h1>React Practical</h1>
     </div>
   );
 }
